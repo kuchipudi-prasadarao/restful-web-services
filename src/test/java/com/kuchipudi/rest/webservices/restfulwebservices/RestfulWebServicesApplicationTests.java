b310ff2665bfd182de40f6e7a647d34e8c434463
kuchipudiprasadarao <kuchipudiprasadarao@localhost>
2025-03-27 22:49:46 +0530

    initial commit

--- src/test/java/com/kuchipudi/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ src/test/java/com/kuchipudi/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kuchipudi.rest.webservices.restful_web_services;
+package com.kuchipudi.rest.webservices.restfulwebservices;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
