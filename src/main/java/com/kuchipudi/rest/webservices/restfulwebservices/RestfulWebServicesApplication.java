449e93750b400aab644d25ea01da21559f5f48da
kuchipudiprasadarao <kuchipudiprasadarao@localhost>
2025-03-27 22:47:31 +0530

    initial commit

--- src/main/java/com/kuchipudi/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/kuchipudi/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,4 +1,4 @@
-package com.kuchipudi.rest.webservices.restful_web_services;
+package com.kuchipudi.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
